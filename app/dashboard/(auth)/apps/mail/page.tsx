82faf411151b04dfa7137901bcd43c84e03c44a0
toby <contact@bundui.com>
2025-07-13 00:52:45 +0300

    feat: mail app added.

--- app/dashboard/(auth)/apps/mail/page.tsx
+++ app/dashboard/(auth)/apps/mail/page.tsx
@@ -1,5 +1,8 @@
+import { cookies } from "next/headers";
 import { generateMeta } from "@/lib/utils";
-import { AlertCircleIcon } from "lucide-react";
+
+import { Mail } from "./components/mail";
+import { accounts, mails } from "./data";
 
 export async function generateMetadata() {
   return generateMeta({
@@ -10,39 +13,22 @@ export async function generateMetadata() {
   });
 }
 
-export default function Page() {
+export default async function MailPage() {
+  const layout = (await cookies()).get("react-resizable-panels:layout:mail");
+  const collapsed = (await cookies()).get("react-resizable-panels:collapsed");
+
+  const defaultLayout = layout ? JSON.parse(layout.value) : undefined;
+  const defaultCollapsed = collapsed ? JSON.parse(collapsed.value) : undefined;
+
   return (
-    <div className="flex h-[90vh] items-center justify-center text-center">
-      <div className="max-w-(--breakpoint-sm) space-y-4">
-        <h1 className="flex items-center justify-center text-3xl font-bold tracking-tight lg:text-4xl">
-          <svg
-            className="mr-5 h-9 w-9 animate-spin"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24">
-            <circle
-              className="opacity-25"
-              cx="12"
-              cy="12"
-              r="10"
-              stroke="currentColor"
-              strokeWidth="4"></circle>
-            <path
-              className="opacity-75"
-              fill="currentColor"
-              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-          </svg>
-          Mail
-        </h1>
-        <div className="">
-          Easily organize incoming and outgoing mail with the mail management template. Quickly
-          track and organize your communications with a user-friendly design.
-        </div>
-        <div className="flex items-center justify-center gap-2 text-muted-foreground">
-          <AlertCircleIcon className="h-4 w-4 text-orange-400" />
-          This page is currently under construction.
-        </div>
-      </div>
+    <div className="h-[calc(100vh-5.8rem)] rounded-md border">
+      <Mail
+        accounts={accounts}
+        mails={mails}
+        defaultLayout={defaultLayout}
+        defaultCollapsed={defaultCollapsed}
+        navCollapsedSize={4}
+      />
     </div>
   );
 }
